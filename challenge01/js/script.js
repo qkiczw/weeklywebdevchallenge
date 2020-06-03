b370eb039e7f8c31a9fe8276b292949dac5b8542
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-06-03 10:51:28 +0200

    Textarea in send email form was added, test code in js was added

--- challenge01/js/script.js
+++ challenge01/js/script.js
@@ -1,1 +1,7 @@
-console.warn('It`s works!');
+// console.warn('It`s works!');
+
+const inputElements = document.querySelectorAll("input[type='checkbox']");
+// console.log(inputElements);
+
+let warn = () => console.log('WARNING!!!')
+inputElements.forEach(el => el.addEventListener('click', warn));
