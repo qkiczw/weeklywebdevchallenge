e80541beec981a9efce3875f681cd01d0e052a89
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-06-08 09:55:35 +0200

    Subscribe form html code was added, JS ontouch listener was added to detect input content on touch displays

--- challenge01/js/script.js
+++ challenge01/js/script.js
@@ -7,3 +7,8 @@ inputElements.forEach(el => el.addEventListener('keyup', function() {
     const inputValue = label.previousElementSibling.value;
     inputValue.length === 0 ? label.classList.remove('hidden') : label.classList.add('hidden');
 }));
+inputElements.forEach(el => el.addEventListener('ontouchend', function() {
+    const label = el.nextElementSibling;
+    const inputValue = label.previousElementSibling.value;
+    inputValue.length === 0 ? label.classList.remove('hidden') : label.classList.add('hidden');
+}));
