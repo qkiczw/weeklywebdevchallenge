3cfab04042342c0e9b5fbc2fb242466b6c86296a
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-06-08 10:20:15 +0200

    Js code to detect input content was rewritred

--- challenge01/js/script.js
+++ challenge01/js/script.js
@@ -2,13 +2,11 @@
 
 const inputElements = document.querySelectorAll(".form__item__input");
 
-inputElements.forEach(el => el.addEventListener('keyup', function() {
-    const label = el.nextElementSibling;
+function inputContentDetect() {
+    const label = this.nextElementSibling;
     const inputValue = label.previousElementSibling.value;
     inputValue.length === 0 ? label.classList.remove('hidden') : label.classList.add('hidden');
-}));
-inputElements.forEach(el => el.addEventListener('ontouchend', function() {
-    const label = el.nextElementSibling;
-    const inputValue = label.previousElementSibling.value;
-    inputValue.length === 0 ? label.classList.remove('hidden') : label.classList.add('hidden');
-}));
+}
+
+inputElements.forEach(el => el.addEventListener('ontouchend', inputContentDetect));
+inputElements.forEach(el => el.addEventListener('keyup', inputContentDetect));
