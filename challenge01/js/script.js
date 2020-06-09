51f5faa24de363c1af4876d7c0fc41d406ebee2f
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-06-09 12:07:47 +0200

    I`m writing code to detect if terms btn was checked and it was btn is not disabled

--- challenge01/js/script.js
+++ challenge01/js/script.js
@@ -10,3 +10,17 @@ function inputContentDetect() {
 
 inputElements.forEach(el => el.addEventListener('ontouchend', inputContentDetect));
 inputElements.forEach(el => el.addEventListener('keyup', inputContentDetect));
+
+const xxx = document.querySelector('#register__terms').value;
+console.log(`value to: ${xxx}`);
+
+const checkbox = document.querySelector('#register__terms');
+
+checkbox.addEventListener('click', function() {
+    const checkboxStatus = document.querySelector('#register__terms').checked;
+    let btn = document.querySelector('#register__button'); 
+    console.log(`Checkbox is: ${checkboxStatus}`);
+
+    checkboxStatus ? btn.disabled = false : btn.disabled = true;
+    console.log(`Button is: ${btn.disabled}`);
+})
